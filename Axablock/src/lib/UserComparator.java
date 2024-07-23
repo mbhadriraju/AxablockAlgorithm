97dd1c430f7770c8b52c3d721f8f87b8683b3337
mbhadriraju <151967712+mbhadriraju@users.noreply.github.com>
2024-07-23 17:19:31 -0500

    Delete Axablock/src/lib directory

--- Axablock/src/lib/UserComparator.java
+++
@@ -1,9 +0,0 @@
-package src.lib;
-class UserComparator implements Comparator<User> {
-    @Override
-    public int compare(User u1, User u2) {
-        return Double.compare(u1.getCurrentBalance(), u2.getCurrentBalance());
-    }
-}
-
-
